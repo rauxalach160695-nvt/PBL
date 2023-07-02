f01faf360a67056cadd3dfa0e6502ddbd9e2e1b0
rauxalach160695-nvt <phong160695@gmail.com>
2023-07-02 07:33:34 +0700

    adddatereturntask

--- src/Routes/tasks.js
+++ src/Routes/tasks.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Task = require("../models/Task");
 const TaskAssignment = require("../models/TaskAssignment");
 const passport = require("passport");
+const Device = require("../models/Device");
+const UserDetail = require("../models/UserDetail");
 router.post("/create", async (req, res) => {
   try {
     const { nameTask, completed, dayStart, dayEnd, deviceId, userId } =
@@ -28,18 +30,39 @@ router.post("/create", async (req, res) => {
 router.post("/gettasks",(passport.authenticate('jwt', {session: false})), async (req, res) => {
   try {
     // const {workSpace } = req.body;
-    console.log(req.body)
+    // console.log(req.body)
     var finalList = []
     var taskList = await Task.find().populate({path: 'userId', model:'User',match:{workSpace:req.user['workSpace']}})
     // var taskList = await Task.find({})
-    console.log(taskList)
+    // console.log(taskList)
     // console.log(typeof(deviceList))
     for(var i =0; i<taskList.length ;i++ ){
       if(taskList[i]['userId'] !== null){
         finalList.push(taskList[i])
       }
     }
-    return res.status(200).json({finalList});
+    listDevice =[]
+    listWorker = []
+    obj= {name:"EERerer"}
+    for(var j = 0; j< finalList.length ;j++ ){
+      var device = await Device.find({_id:finalList[j]['deviceId']})
+      var worker = await UserDetail.find({userId:finalList[j]['userId']['_id']})
+      listDevice.push(device[0]['nameDevice'])
+      listWorker.push(worker[0])
+      console.log(device[0]['nameDevice'])
+      // finalList[j]['nameDevice'] = device[0]['nameDevice']
+      // finalList[j]['nameDevice'] = '123'
+
+      // Object.defineProperty(finalList[j], 'deviceName', {
+      //   value: device['nameDevice'],
+      //   writable: true,
+      //   enumerable: true,
+      //   configurable: true
+      // });
+    }
+   
+    console.log(listWorker)
+    return res.status(200).json({finalList,listDevice,listWorker});
   } catch (err) {
     return res.status(500).json(err);
   }
